6d3b1ae90ce1194651cb092524f34d76e87204e2
ZacCat <catalanoz15@students.ecu.edu>
2017-05-09 20:07:54 -0400

    Python file to read in a dataset

---
+++ python/readDataSet.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3.5
+
+# Training method eg. Train by class, or Train by Plugin
+trainMethod = 'Class'
+# Desired WP Version
+version = '4.3'
+
+# Generate the file location, given Features, fMatrix, Labels, lMatrix 
+def genLoc( str ):
+	return '/home/zac/corpus/training/MultiLabel/TrainBy' + trainMethod + "-" + str + '-' + version + '.txt'
+
+# Read in either array of either:
+# Feature names ( str = Features )
+# Label names ( str = Labels )
+def genList( str ):
+	s = open(genLoc( str )).readline()
+	s = s[1 : len(s) - 1].replace('\"', '')
+	return s.split(',')
+
+# Read in either:
+# Feature Matrix ( str = fMatrix )
+# Label Matrix ( str = lMatrix )
+def genMatrix( str ):
+	s = open(genLoc( str )).readline()
+	s = s[2 : len(s) - 2]
+	lst = s.split('],[')
+
+	m = []
+
+	for item in lst:
+			m.append(item.split(','))
+
+	return m
