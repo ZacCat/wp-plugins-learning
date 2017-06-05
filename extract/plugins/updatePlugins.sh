37042ce41fb13b29ecefc98b527ce70ead04bae3
ZacCat <catalanoz15@students.ecu.edu>
2017-06-05 13:31:18 -0400

    Changed download method from curl to svn

--- extract/plugins/updatePlugins.sh
+++ extract/plugins/updatePlugins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl http://plugins.svn.wordpress.org | grep '  <li><a href=' | sed 's;  <li><a href=".*">\([^<]*\)<.*;\1;' | sed 's;/;;' > plugins.txt
+svn ls http://plugins.svn.wordpress.org | sed 's/\/$//' >> plugins.txt
