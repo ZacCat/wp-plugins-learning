345b9bdba48b98ace7fe0d9405802bf8ef34a857
ZacCat <catalanoz15@students.ecu.edu>
2017-06-06 11:22:12 -0400

    Minor Changes
    Removed some files, and small changes to extraction script

--- extract/plugins/getAllPlugins.sh
+++ extract/plugins/getAllPlugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 minV=4
-minY=2015
+minY=2016
 
 while read p; do
 
@@ -12,7 +12,7 @@ while read p; do
   if [[ $e1 -ge $minY && $e2 -ge $minV ]]; then
     mkdir $p
     svn co http://plugins.svn.wordpress.org/$p/trunk $p
-  else
-  	echo "Skipping $p"
+else
+  	echo "Skipping $p Modified : $e1 Tested : $e2"
   fi
 done<plugins.txt
